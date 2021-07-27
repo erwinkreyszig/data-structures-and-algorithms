c7eec8213ebe93f00a18a22a7aad0539e6329387
oscar <vergil.ferrer.jp@gmail.com>
2021-07-27 21:47:28 +0900

    changed to binary search tree

--- data-structures/binary_search_tree.py
+++ data-structures/binary_search_tree.py
@@ -18,13 +18,37 @@ class BinarySearchTree(object):
             current_node.left = self.__add(current_node.left, value)
         elif current_node.value < value:
             current_node.right = self.__add(current_node.right, value)
-        else:
-            return current_node
         return current_node
 
     def add(self, value):
         self.root = self.__add(self.root, value)
 
+
+    def __contains(self, current_node, value):
+        if current_node is None:
+            return False
+        if current_node.value > value:
+            return self.__contains(current_node.left, value)
+        elif current_node.value < value:
+            return self.__contains(current_node.right, value)
+        else:
+            return True
+
+    def contains(self, value):
+        return self.__contains(self.root, value)
+
+    def __delete(self, current_node, value):
+        if current_node is None:
+            return None
+        if current_node.value > value:
+            current_node.left = self.__delete(current_node.left, value)
+            return current_node
+        elif current_node.value < value:
+            current_node.right = self.__delete(current_node.right, value)
+            return current_node
+        else:
+            # TODO
+            pass
 
 if __name__ == '__main__':
     pass
